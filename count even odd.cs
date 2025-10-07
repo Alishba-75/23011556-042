5fcfee49d8a421c403b85716f28c534f91377ebd
Alishba-75 <alishbamazhariqbal57@gmail.com>
2025-10-07 14:08:06 +0500

    Update count even odd.cs

--- count even odd.cs
+++ count even odd.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace _23011556_042
+public class count_even_odd
 {
-    internal class count_even_odd
+    public void count()
     {
+        int[] arr = new int[10];
+        int even = 0, odd = 0;
+
+        Console.WriteLine("Enter 10 numbers:");
+        for (int i = 0; i < 10; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+            if (arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        Console.WriteLine($"Even numbers = {even}");
+        Console.WriteLine($"Odd numbers = {odd}");
     }
 }
